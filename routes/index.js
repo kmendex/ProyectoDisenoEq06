e929f6a6e4ea40f6d3428fadc1e9036da63bf428
Kenneth <kmendez@ic-itcr.ac.cr>
2020-10-22 20:54:03 -0600

    pegadaBase

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED='0';
+var DATABASE_URL = 'postgres://wdupoeegmlpkfn:c144a4e4b7f388899becdc70f091509be22890ec8f115201527460c33d477c14@ec2-54-90-68-208.compute-1.amazonaws.com:5432/drs6db4nh7o0v';
+
 var express = require('express');
 var router = express.Router();
 
+const { Client } = require('pg');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//
+  const client = new Client({connectionString: DATABASE_URL, ssl:true});
+  client.connect();
+  client.query('select textaqui from test where id = 1', (err, res1) => {
+      if (err) throw err
+      res.render('index', { title: res1.rows[0].textaqui });
+  client.end();
+  });
+//
 });
 
 module.exports = router;
