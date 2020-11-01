0214cfaa971347598c96a34d922472f354fc06ac
JAMares <49013556+JAMares@users.noreply.github.com>
2020-11-01 01:55:39 -0600

    Add files via upload

---
+++ ts/menu.ts
@@ -0,0 +1,15 @@
+(function(){
+    const menu = document.querySelector('.menu');
+    const menuNavegacion = document.querySelector('.menu-navegacion');
+
+    menu?.addEventListener('click', ()=>{
+        menuNavegacion?.classList.toggle("spread");
+    });
+
+    window.addEventListener('click', e=>{
+        if(menuNavegacion?.classList.contains('spread') && e.target != menuNavegacion && e.target != menu){
+            menuNavegacion?.classList.toggle("spread");
+        }
+    });
+
+})();
