22fdca9820a1436834acf7bd0456bf2516374479
Tony <tony.ulloam@gmail.com>
2020-11-06 19:24:03 -0600

    Cambios clases

--- models/Direccion.ts
+++ models/Direccion.ts
@@ -19,20 +19,51 @@ export interface Districto {
 
 export class Direccion { 
   //field 
-  provincia: Provincia;
-  canton: Canton;
-  districto: Districto;
-  info: string; 
+  private _provincia: Provincia;
+  private _canton: Canton;
+  private _districto: Districto;
+  private _info: string; 
   
   //constructor 
   constructor(provincia: Provincia, canton: Canton, districto: Districto, info: string) {
-    this.provincia = provincia;
-    this.canton = canton;
-    this.districto = districto;
-    this.info = info;
-  }  
+    this._provincia = provincia;
+    this._canton = canton;
+    this._districto = districto;
+    this._info = info;
+  }
   
-  //function 
+  //function
+  get provincia() : Provincia{
+    return this._provincia;
+  }
+
+  set provincia(provincia: Provincia){
+    this._provincia = provincia;
+  }
+
+  get canton() : Canton{
+    return this._canton;
+  }
+
+  set canton(canton: Canton){
+    this._canton = canton;
+  }
+
+  get districto() : Districto{
+    return this._districto;
+  }
+
+  set districto(districto: Districto){
+    this._districto = districto;
+  }
+
+  get info() : string{
+    return this._info;
+  }
+
+  set info(info: string){
+    this._info = info;
+  } 
 }
 
 let provinciaV:Provincia = {id: 1, nombre: "Heredia"};
--- models/Persona.ts
+++ models/Persona.ts
@@ -9,21 +9,69 @@ export enum eEstado {
 
 export class Persona { 
   //field 
-  identificacion :number;
-  nombreCompleto: string;
-  celular: number;
-  correo: string;
-  direccion: Direccion;
-  estado: eEstado;
+  private _identificacion:number;
+  private _nombreCompleto: string;
+  private _celular: number;
+  private _correo: string;
+  private _direccion: Direccion;
+  private _estado: eEstado;
   
   //constructor 
-  constructor(identificacion: number, nombreCompleto: string, celular: number, correo: string, direccion: Direccion, estado: eEstado) {
-    this.identificacion = identificacion;
-    this.nombreCompleto = nombreCompleto;
-    this.celular = celular;
-    this.correo = correo;
-    this.direccion = direccion;
-    this.estado = estado;
+  constructor(identificacion: number, nombreCompleto: string, celular?: number, correo?: string, direccion?: Direccion, estado?: eEstado) {
+    this._identificacion = identificacion;
+    this._nombreCompleto = nombreCompleto;
+    this._celular = celular;
+    this._correo = correo;
+    this._direccion = direccion;
+    this._estado = estado;
+  }
+
+  get identificacion (): number{
+    return this._identificacion;
+  }
+
+  set identificacion (id: number) {
+    this._identificacion = id;
+  }
+
+  get nombreCompleto (): string{
+    return this._nombreCompleto;
+  }
+
+  set nombreCompleto (nombre: string) {
+    this._nombreCompleto = nombre;
+  }
+
+  get celular (): number{
+    return this._celular;
+  }
+
+  set celular (celular: number) {
+    this._celular = celular;
+  }
+
+  get correo (): string{
+    return this._correo;
+  }
+
+  set correo (correo: string) {
+    this._correo = correo;
+  }
+
+  get direccion (): Direccion{
+    return this._direccion;
+  }
+
+  set direccion (direccion: Direccion) {
+    this._direccion = direccion;
+  }
+
+  get estado (): eEstado{
+    return this._estado;
+  }
+
+  set estado (estado: eEstado) {
+    this._estado = estado;
   }
 
   toString() {
@@ -37,5 +85,7 @@ let districtoV:Districto = {id: 1, nombre: "Calle Chaves"};
 let direccion = new Direccion(provinciaV, cantonV, districtoV, "Que bonito lugar");
 
 let persona = new Persona(1111, "Tony", 12345678, "tony@gmail.com", direccion, eEstado.activo);
+
+let persona1 = new Persona(1, "Juan"); 
 
 //console.log(persona.nombreCompleto);
