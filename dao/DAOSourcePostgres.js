72881f812feea9be686b0e4f0871857946e121cf
Kenneth <kmendez@ic-itcr.ac.cr>
2020-11-09 16:07:19 -0600

    data++

--- dao/DAOSourcePostgres.js
+++ dao/DAOSourcePostgres.js
@@ -9,30 +9,85 @@ const pool = new Pool({
 });
 
 module.exports = {
+        // Retornan un json o un null
 
+        // { "name":"...","juridicalCode":"...","description":"...","logoURL":"...","country":"...",
+        //   "province":"...","canton":"...","district":"...","address":"..." }
         getOrganization : async function (orgCode, callback){ 
                 let values = [orgCode];
                 let result = await pool.query('SELECT "readOrganization"($1)', values, callback);
-                return result.rows[0];
+                return result.rows[0].readOrganization;
         },
+        // { "code":"...","name":"...","creationDate":"...","UnitType":"..." }
         getCoordination : async function (orgCode, callback){
                 let values = [orgCode];
                 let result = await pool.query('SELECT "readCoordination"($1)', values, callback);
-                return result.rows[0];
+                return result.rows[0].readCoordination;
         },
+        // [ { "code":"...","name":"...","creationDate":"...","UnitType":"..."}, ... ]
         getZone : async function (crdCode, callback){
                 let values = [crdCode];
                 let result = await pool.query('SELECT "readZone"($1)', values, callback);
-                return result.rows[0];
+                return result.rows[0].readZone;
         },
+        // [ { "code":"...","name":"...","creationDate":"...","UnitType":"..."}, ...]
         getBranch : async function (zonCode, callback){
                 let values = [zonCode];
                 let result = await pool.query('SELECT "readBranch"($1)', values, callback);
-                return result.rows[0];
+                return result.rows[0].readBranch;
         },
+        // [ { "code":"...","name":"...","creationDate":"...","UnitType":"..."}, ... ]
         getGroup : async function (zonCode, brnCode, callback){
                 let values = [zonCode, brnCode];
                 let result = await pool.query('SELECT "readGroup"($1,$2)', values, callback);
-                return result.rows[0];
+                return result.rows[0].readGroup;
+        },
+        // [ { "identificationCode":"...","fullName":"...","rol":"...","state":"..." }, ... ]
+        getMembers : async function (grpCode, callback){
+                let values = [grpCode];
+                let result = await pool.query('SELECT "readMembers"($1)', values, callback);
+                return result.rows[0].readMembers;
+        },
+        // [ { "type":2,"name":"...","value":"..."}, ... ]
+        // type: 2:telefono | 4:correo | 5:web | 6:facebook | 7:twitter         
+        getContactOrganization : async function (orgCode, callback){
+                let values = [orgCode];
+                let result = await pool.query('SELECT "readContactOrganization"($1)', values, callback);
+                return result.rows[0].readContactOrganization;
+        },
+        // { "fullName":"...","identificationCode":"...","birthdate":"...","state":"...","country":"...",
+        //   "province":"...","canton":"...","district":"...","address":"..."}
+        getPerson : async function(prsIdentification, callback){
+                let values = [prsIdentification];
+                let result = await pool.query('SELECT "readPerson"($1)', values, callback);
+                return result.rows[0].readPerson;
+        },
+        // [ {"type":1,"name":"...","value":"..."}, ...]
+        // type: 1:telefono | 3:correo
+        getContactPerson : async function(prsIdentification, callback){
+                let values = [prsIdentification];
+                let result = await pool.query('SELECT "readContactPerson"($1)', values, callback);
+                return result.rows[0].readContactPerson;
+        },
+        // [ {"id...":1,"name":"..."}, ...]
+        getCountries : async function(callback){
+                let result = pool.query('SELECT "readCountries"()', callback);
+                return result.rows[0].readCountries;
+        },
+        getProvinces : async function (ctrName, callback){
+                let values = [ctrName];
+                let result = pool.query('SELECT "readProvinces"($1)', values, callback);
+                return result.rows[0].readProvinces;
+        },
+        getCantons : async function (prvName, callback){
+                let values = [prvName];
+                let result = pool.query('SELECT "readCantons"($1)', values, callback);
+                return result.rows[0].readCantons;
+        },
+        getDistricts : async function (ctnName, callback){
+                let values = [ctnName];
+                let result = pool.query('SELECT "readDistricts"($1)', values, callback);
+                return result.rows[0].readDistricts;
         }
+
 };
